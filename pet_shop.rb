e9abcb68c6773008b5e02de51947db0338a95ace
victor chugbo <vcmails2010@gmail.com>
2018-04-02 11:53:39 +0100

    more function testing

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,3 +1,40 @@
 def pet_shop_name(pet_shop)
   return pet_shop[:name]
 end
+
+def total_cash(pet_shop)
+  return pet_shop[:admin][:total_cash]
+end
+
+# def add_or_remove_cash(pet_shop, cash)
+#   cash = pet_shop[:admin][:total_cash]
+#   return cash += number
+# end
+
+#def add_or_remove_cash__add(pet_shop)
+#   pet_shop.each do |key, value|
+#     if key == admin
+#
+#     end
+#   return admin[total_cash] -= 10
+#   return admin[total_cash]
+# end
+# end
+
+def pets_sold(pet_shop)
+  return pet_shop[:admin][:pets_sold]
+end
+
+# def increase_pets_sold(pet_shop, num)
+#   return pet_shop[:admin][:pets_sold].fetch + 2
+# end
+
+def all_pets_by_breed
+  pet_shop[pets].each {| n, pt, br, pr|}
+  breed = br
+  if breed == breed |pets_by_breed|
+
+  end
+end
+
+return pets_by_breed.count
